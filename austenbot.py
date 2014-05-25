497735349c67168d4803d697a8ae1ac2703a0ac9
Alisha Ukani <alishaaukani@gmail.com>
2014-05-25 08:57:15 -0400

    Publish to Twitter

--- austenbot.py
+++ austenbot.py
@@ -94,8 +94,8 @@ print message
 print calculateChars(tweet)
 
 # Publish to Twitter
-#auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-#auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-#api = tweepy.API(auth)
-#api.update_status(message)
+api = tweepy.API(auth)
+api.update_status(message)
